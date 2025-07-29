ebf2cba231e8d5abc700e2051a7fcc6506285bc1
Kuno_Kuo <kuno.kuo@gmail.com>
2025-07-29 18:07:27 +0800

    test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,7 +2,7 @@ import pytest
 from app.calculator import add, subtract, divide, multiply
 
 @pytest.mark.parametrize("x, y, expected", [
-    (1, 2, 3),
+    (2, 2, 3),
     (-1, 1, 0),
     (0, 0, 0),
     (100, 200, 300),
