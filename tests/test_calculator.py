0461e12968d72419facd1c3e2641f975183e14fa
Kuno_Kuo <kuno.kuo@gmail.com>
2025-08-04 10:04:07 +0800

    Modified.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -11,7 +11,7 @@ def test_add(x, y, expected):
     assert add(x, y) == expected
 
 @pytest.mark.parametrize("x, y, expected", [
-    (5, 3, 2),
+    (5, 4, 1),
     (3, 5, -2),
     (0, 0, 0),
     (-1, -1, 0),
